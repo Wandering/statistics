172585fbc4b5a36e64509224d697b32d0f41eefe
yyp <wuhun0301@qq.com>
2016-03-19 12:14:47 +0800

    初步合并版本

---
+++ agents-api/src/main/java/cn/thinkjoy/agents/IGoodsCheck.java
@@ -0,0 +1,8 @@
+package cn.thinkjoy.agents;
+
+/**
+ * Created by admin on 2016/3/19.
+ */
+public interface IGoodsCheck {
+    void goodsCheck();
+}
---
+++ agents-service/src/main/java/cn/thinkjoy/agents/service/ex/impl/IGoodsCheckImpl.java
@@ -0,0 +1,13 @@
+package cn.thinkjoy.agents.service.ex.impl;
+
+import cn.thinkjoy.agents.IGoodsCheck;
+
+/**
+ * Created by admin on 2016/3/19.
+ */
+public class IGoodsCheckImpl implements IGoodsCheck {
+    @Override
+    public void goodsCheck() {
+        System.out.println("test");
+    }
+}
