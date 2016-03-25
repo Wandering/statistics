7a3ab205717d8ca0b02b2cd3554879a197be646d
dengshaofei <dengsf007@gmail.com>
2016-03-25 18:16:52 +0800

    commit

---
+++ statistics-web-war/src/main/webapp/dist/static/scripts/index/goodsManager/outbound_flow_area.js
@@ -0,0 +1,40 @@
+/**
+ * Created by kepeng on 15/9/9.
+ */
+define('static/scripts/index/goodsManager/outbound_flow_area', [], function(require, exports, module) {
+    function tip(ele, str) {
+        var errorLable = ele.find('p');
+        errorLable.html(str);
+        errorLable.show(500);
+        setTimeout(function() {
+            errorLable.hide(500);
+        }, 2000)
+    }
+
+    if (typeof String.prototype.trim !== 'function') {
+        String.prototype.trim = function() {
+            return this.replace(/(^\s*)|(\s*$)/g,'');
+        };
+    }
+
+    function validateForm(callback) {
+
+        alert(88)
+
+        var flowAreaListLen =  $('#flow-area-list [type="checkbox"]:checked').length;
+        if(flowAreaListLen=='0'){
+            tip($('#flow-area-list').parent(), '至少选择一个流向地');
+            return false;
+        }
+
+
+        callback([""]);
+    }
+
+
+    module.exports = {
+        validate: function(callback) {
+            validateForm(callback);
+        }
+    }
+});
