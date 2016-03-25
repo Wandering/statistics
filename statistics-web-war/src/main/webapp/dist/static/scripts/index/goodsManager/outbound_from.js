07628ef0e8df81984181cc5d94f5bc0d618860fa
dengshaofei <dengsf007@gmail.com>
2016-03-25 15:57:08 +0800

    commit

---
+++ statistics-web-war/src/main/webapp/dist/static/scripts/index/goodsManager/outbound_from.js
@@ -0,0 +1,39 @@
+/**
+ * Created by kepeng on 15/9/9.
+ */
+define('static/scripts/index/goodsManager/outbound_from', [], function(require, exports, module) {
+    function tip(ele, str) {
+        var errorLable = ele.find('p');
+        errorLable.html(str);
+        errorLable.show(500);
+        setTimeout(function() {
+            errorLable.hide(500);
+        }, 2000)
+    }
+
+    if (typeof String.prototype.trim !== 'function') {
+        String.prototype.trim = function() {
+            return this.replace(/(^\s*)|(\s*$)/g,'');
+        };
+    }
+
+    function validateForm(callback) {
+
+
+        var  areaV = $('#dep_provinces').find('option:selected').val();
+        var  simpleCode = $('#dep_provinces').find('option:selected').attr('simpleCode');
+
+        if (areaV=="") {
+            tip($('#dep_provinces').parent().parent(), '请选择流向地');
+            return;
+        }
+        callback([simpleCode]);
+    }
+
+
+    module.exports = {
+        validate: function(callback) {
+            validateForm(callback);
+        }
+    }
+});
