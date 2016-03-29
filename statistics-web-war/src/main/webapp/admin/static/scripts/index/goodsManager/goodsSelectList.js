f39b54cd6ee09456ba10b4ab86482dc6b4ba6cb4
dengshaofei <dengsf007@gmail.com>
2016-03-29 10:36:55 +0800

    commit

--- statistics-web-war/src/main/webapp/admin/static/scripts/index/goodsManager/goodsSelectList.js
+++ statistics-web-war/src/main/webapp/admin/static/scripts/index/goodsManager/goodsSelectList.js
@@ -168,7 +168,6 @@ define(function (require, exports, module) {
                 success: function (data) {
                     console.log(data)
                     succCallback(data);
-
                 },
                 beforeSend: function (xhr) {
                 },
