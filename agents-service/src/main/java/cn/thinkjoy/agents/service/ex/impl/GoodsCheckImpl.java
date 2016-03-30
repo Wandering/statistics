179c007027f0314cdc5fd380ee2ded265cdb7d01
yyp <wuhun0301@qq.com>
2016-03-30 16:10:02 +0800

    解除货号规则、号段查询bug

--- agents-service/src/main/java/cn/thinkjoy/agents/service/ex/impl/GoodsCheckImpl.java
+++ agents-service/src/main/java/cn/thinkjoy/agents/service/ex/impl/GoodsCheckImpl.java
@@ -24,30 +24,70 @@ public class GoodsCheckImpl implements IGoodsCheck {
         Integer status=0;
         if(map.containsKey("cardNumber")) {
             String cardNumber=map.get("cardNumber").toString();
-            String goodsNumber=AgentsInfoUtils.addZeroForNum(cardExDAO.selectCardByUid(cardNumber), 6);
-        if(map.containsKey("userId")) {
-            Map<String,Object> userArea=cardExDAO.selectByUid(map.get("userId").toString());
-            if(userArea.containsKey("countyId")){
-                if(goodsNumber.equals(userArea.get("countyId"))){
-                    status=0;
-                }else {
-                    status=1;
+            String selgoodsNumber=cardExDAO.selectCardByUid(cardNumber);
+            String goodsNumber=AgentsInfoUtils.addZeroForNum(selgoodsNumber, 6);
+            if(lastAgentsArea(selgoodsNumber)) {
+                if (map.containsKey("userId")) {
+                    Map<String, Object> userArea = cardExDAO.selectByUid(map.get("userId").toString());
+                    if (userArea.containsKey("countyId")) {
+                        if (goodsNumber.equals(userArea.get("countyId"))) {
+                            status = 0;
+                        } else {
+                            status = 1;
+                        }
+                    } else if (userArea.containsKey("cityId")) {
+                        if (goodsNumber.equals(userArea.get("cityId"))) {
+                            status = 0;
+                        } else {
+                            status = 1;
+                        }
+                    } else if (userArea.containsKey("provinceId")) {
+                        if (goodsNumber.equals(userArea.get("provinceId"))) {
+                            status = 0;
+                        } else {
+                            status = 1;
+                        }
+                    }
+
                 }
-            }else if(userArea.containsKey("cityId")){
-                if(goodsNumber.equals(userArea.get("cityId"))){
-                    status=0;
-                }else {
-                    status=1;
-                }
-            }else if(userArea.containsKey("provinceId")){
-                if(goodsNumber.equals(userArea.get("provinceId"))){
-                    status=0;
-                }else {
-                    status=1;
+            }else {
+                Map<String, Object> userArea = cardExDAO.selectByUid(map.get("userId").toString());
+                switch (selgoodsNumber.length()) {
+                    case 0:
+                        status = 1;
+                        break;
+                    case 2:
+                        if (userArea.containsKey("provinceId")) {
+                            if (goodsNumber.equals(userArea.get("provinceId"))) {
+                                status = 0;
+                            } else {
+                                status = 1;
+                            }
+                        }
+                        break;
+                    case 4:
+                        if (userArea.containsKey("cityId")) {
+                            if (goodsNumber.equals(userArea.get("cityId"))) {
+                                status = 0;
+                            } else {
+                                status = 1;
+                            }
+                        }
+                        break;
+                    case 6:
+                        if (userArea.containsKey("countyId")) {
+                            if (goodsNumber.equals(userArea.get("countyId"))) {
+                                status = 0;
+                            } else {
+                                status = 1;
+                            }
+                        }
+                        break;
+                    default:
+                        status = 1;
+                        break;
                 }
             }
-
-        }
 
             condition.put("activeDate", System.currentTimeMillis());
             condition.put("errorStatus", status);
@@ -55,4 +95,15 @@ public class GoodsCheckImpl implements IGoodsCheck {
             cardExDAO.active(condition);
         }
     }
+
+    /**
+     * 判断当前卡所在代理商有没有下级
+     * @param goodsNumber
+     * @return
+     */
+    private boolean lastAgentsArea(String goodsNumber){
+        return cardExDAO.lastAgentsArea(goodsNumber)>0;
+    }
+
+
 }
